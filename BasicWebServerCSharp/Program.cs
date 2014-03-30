00e2817accbaca3c580b848991c4a5b63604cec8
Andrew Weeks <adweeks@gmail.com>
2014-03-30 11:32:07 -0600

    router ip can now be specified

--- BasicWebServerCSharp/Program.cs
+++ BasicWebServerCSharp/Program.cs
@@ -9,6 +9,9 @@ namespace BasicWebServerCSharp
     class Program { 
         static void Main(string[] args) {
             SmallServer.Server server = new SmallServer.Server();
+            server.PhysicalPath = @"C:\web\workspace";
+            server.Prefixes.Add("http://" + server.GetRouterIp() + ":8081/");
+            Console.WriteLine("http://" + server.GetRouterIp() + "/");
             server.Start();
         }
         
--- SmallServer/Class1.cs
+++ SmallServer/Class1.cs
@@ -48,34 +48,49 @@ namespace SmallServer
             get { return _physicalpath; }
             set { _physicalpath = value; }
         }
+
+        private List<string> _prefixes = new List<string>() { 
+            "http://localhost:8081/", 
+            "http://127.0.0.1:8081/"
+        };
+
+        public List<string> Prefixes
+        {
+            get { return _prefixes; }
+            set { _prefixes = value; }
+        }
         
+        /// <summary>
+        /// Method to start listening to the requested prefix
+        /// </summary>
         public void Start() {
             Console.WriteLine("Small Server Starting...");
             Console.WriteLine("Adding Prefixes...");
-            this.Listener.Prefixes.Add("http://localhost:8081/");
-            this.Listener.Prefixes.Add("http://127.0.0.1:8081/");
-            this.Listener.Start();
-            
-            foreach (var prefix in this.Listener.Prefixes)
+            foreach (var prefix in this.Prefixes)
             {
+                this.Listener.Prefixes.Add(prefix);
                 Console.WriteLine("Listening for requests on {0}", prefix);
             }
-            
-            
+            this.Listener.Start();
+
             while (true)
             {
+                //Set the current context to the listener context.
                 this.Context = this.Listener.GetContext();
-                string page = this.Context.Request.Url.LocalPath; //.Replace("/", ""); 
+                //Get the page requested.
+                string page = this.Context.Request.Url.LocalPath;
+                //Get any query parameters that were passed along (get)
                 string query = this.Context.Request.Url.Query.Replace("?", "");
                 Console.WriteLine("Received request for {0}?{1}", page, query);
 
-                //read post paramaters
+                //read anything that was included in the body (ie json)
                 StreamReader inputStream = new StreamReader(this.Context.Request.InputStream);
-                string data = inputStream.ReadToEnd();
+                string bodyData = inputStream.ReadToEnd();
 
                 //write response headers
                 this.Context.Response.Headers.Add("SmallServer","1.0");
-                //write output
+
+                //write output response
                 var fileBytes = this.GetFile(page);
                 this.Context.Response.OutputStream.Write(fileBytes,0,fileBytes.Length);
                 this.Context.Response.OutputStream.Flush();
@@ -87,17 +102,47 @@ namespace SmallServer
         {
             if (localPath.EndsWith("/"))
                 localPath = localPath + "index.html";
+            Console.WriteLine("Fufilling Request...");
             try
             {
                 this.Context.Response.Headers.Add("Content-Type", getMimeFromFile(this.PhysicalPath + localPath));
                 return System.IO.File.ReadAllBytes(this.PhysicalPath + localPath);
             }
-            catch (Exception ex) 
+            catch 
             {
+                Console.WriteLine("Error Occured With Request For:{0}", localPath);
                 return System.Text.Encoding.UTF8.GetBytes("<html><head></head><body>Small Server <br/> Sorry, Error Occured. <br/> :(</body></html>");
             }
         }
 
+        public string GetRouterIp()
+        {
+            try
+            {
+                ////http://www.fluxbytes.com/csharp/how-to-get-your-ip-in-c/
+                //IPAddress localAddress = Dns.GetHostEntry(Dns.GetHostName()).AddressList.FirstOrDefault(ip => ip.AddressFamily == AddressFamily.InterNetwork);
+
+                //if (localAddress != null)
+                //{
+                //    return localAddress.ToString();
+                //}
+                IPHostEntry host;
+                string localIP = "?";
+                host = Dns.GetHostEntry(Dns.GetHostName());
+                foreach (IPAddress ip in host.AddressList)
+                {
+                    if (ip.AddressFamily.ToString() == "InterNetwork")
+                    {
+                        localIP = ip.ToString();
+                    }
+                }
+                return localIP;
+            }
+            catch
+            {
+                throw;
+            }
+        }
         /// <summary>
         /// Ensures that file exists and retrieves the content type 
         /// </summary>
